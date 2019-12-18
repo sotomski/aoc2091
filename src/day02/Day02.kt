fc282f8a8ab7b11508d04d11553a8ce2e0970ca9
Karol Sotomski <karol.sotomski@gmail.com>
2019-12-18 11:20:22 +0100

    Day 2 before refactoring

---
+++ src/day02/Day02.kt
@@ -0,0 +1,63 @@
+package day02
+
+fun main() {
+    val initialMemory = arrayListOf(1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,9,19,23,1,6,23,27,1,10,27,31,1,5,31,35,2,6,35,39,1,5,39,43,1,5,43,47,2,47,6,51,1,51,5,55,1,13,55,59,2,9,59,63,1,5,63,67,2,67,9,71,1,5,71,75,2,10,75,79,1,6,79,83,1,13,83,87,1,10,87,91,1,91,5,95,2,95,10,99,2,9,99,103,1,103,6,107,1,107,10,111,2,111,10,115,1,115,6,119,2,119,9,123,1,123,6,127,2,127,10,131,1,131,6,135,2,6,135,139,1,139,5,143,1,9,143,147,1,13,147,151,1,2,151,155,1,10,155,0,99,2,14,0,0)
+
+    val result = executeIntcode(initialMemory, 12, 2)
+
+    println("Result: $result")
+}
+
+fun executeIntcode(initialMemory: ArrayList<Int>, noun: Int, verb: Int): Int {
+    // Initialize memory
+    val memory = ArrayList(initialMemory)
+    memory[1] = noun
+    memory[2] = verb
+
+    var cursor = 0
+    fun firstOperandPosition() = memory[cursor + 1]
+    fun secondOperandPosition() = memory[cursor + 2]
+    fun resultPosition() = memory[cursor + 3]
+
+    var operation = 0
+    var firstOperand = 0
+    var secondOperand = 0
+    var result = 0
+
+    println("===> EXECUTING PROGRAM: $memory")
+
+    loop@ while(operation != 99) {
+//        println("Iteration start: $memory")
+
+        // TODO: Read values
+        operation = memory[cursor]
+
+        // TODO: Perform operation
+        result = when(operation) {
+            1 -> {
+                firstOperand = memory[firstOperandPosition()]
+                secondOperand = memory[secondOperandPosition()]
+                firstOperand + secondOperand
+            }
+            2 -> {
+                firstOperand = memory[firstOperandPosition()]
+                secondOperand = memory[secondOperandPosition()]
+                firstOperand * secondOperand
+            }
+            99 -> break@loop
+            else -> throw UnsupportedOperationException()
+        }
+
+        memory[resultPosition()] = result
+
+        // TODO: Step forward (update positions)
+        cursor += 4
+
+//        println("Iteration end: $memory")
+
+    }
+
+    println("===> FINISHED EXECUTION")
+
+    return memory[0]
+}
