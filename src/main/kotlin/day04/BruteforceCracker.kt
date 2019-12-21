c205feb6ee2e56cdf84be1427c8ee837d9c2b3ed
Karol Sotomski <karol.sotomski@gmail.com>
2019-12-21 16:42:18 +0100

    Solve part 2 day 4

--- src/main/kotlin/day04/BruteforceCracker.kt
+++ src/main/kotlin/day04/BruteforceCracker.kt
@@ -4,6 +4,46 @@ class BruteforceCracker(private val lower: Int, private val upper: Int) {
 
     fun passwordCount() = (lower..upper).filter { isPassword(it) }.distinct().count()
 
+    fun passwordCountWithStrictPairs() = (lower..upper).filter { isPasswordWithStrictPairs(it) }.distinct().count()
+
+    fun isPasswordWithStrictPairs(candidate: Int): Boolean {
+        val digits = candidate.toString().toCharArray().map { it.toInt() }
+
+        // Must be 6 digits
+        if (digits.count() != 6) {
+            return false
+        }
+
+        // Must have at least one pair that's not part of a bigger group
+        val pairCounts = mutableListOf(1)
+        var pairIndex = 0
+
+        digits.windowed(2, 1).forEach {
+            val prev = it[0]
+            val next = it[1]
+
+            if (prev == next) {
+                pairCounts[pairIndex] = pairCounts[pairIndex] + 1
+            } else {
+                pairCounts.add(1)
+                pairIndex++
+            }
+        }
+
+        if (!pairCounts.contains(2)) {
+            return false
+        }
+
+        // Must be monotonic
+        digits.windowed(2, 1).forEach {
+            if (it[0] > it[1]) {
+                return false
+            }
+        }
+
+        return true
+    }
+
     fun isPassword(candidate: Int): Boolean {
         val digits = candidate.toString().toCharArray().map { it.toInt() }
 
@@ -19,7 +59,7 @@ class BruteforceCracker(private val lower: Int, private val upper: Int) {
         }
 
         // Must be monotonic
-        digits.windowed(2, 1 ).forEach {
+        digits.windowed(2, 1).forEach {
             if (it[0] > it[1]) {
                 return false
             }
--- src/test/kotlin/day04/BruteforceCrackerTest.kt
+++ src/test/kotlin/day04/BruteforceCrackerTest.kt
@@ -5,7 +5,7 @@ import org.junit.Test
 
 class BruteforceCrackerTest {
     @Test
-    fun `trivial cases`() {
+    fun `should handle part 1`() {
         val cracker = BruteforceCracker(111111, 999999)
 
         (111111..999999 step 111111).forEach {
@@ -14,5 +14,14 @@ class BruteforceCrackerTest {
 
         assertFalse("223450 is not a valid password", cracker.isPassword(223450))
         assertFalse("123789 is not a valid password", cracker.isPassword(123789))
+    }
+
+    @Test
+    fun `should handle part 2`() {
+        val cracker = BruteforceCracker(111111, 999999)
+
+        assertTrue("$112233 should be a valid password", cracker.isPasswordWithStrictPairs(112233))
+        assertTrue("111122 should be a valid password", cracker.isPasswordWithStrictPairs(111122))
+        assertFalse("123444 is not a valid password", cracker.isPasswordWithStrictPairs(123444))
     }
 }
