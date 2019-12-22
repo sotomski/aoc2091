0d8975abaea712d52ef71fb487527e09e5542b0f
Karol Sotomski <karol.sotomski@gmail.com>
2019-12-22 14:16:12 +0100

    Add immediate mode for instruction parameters

--- src/main/kotlin/day05/IntcodeComputer.kt
+++ src/main/kotlin/day05/IntcodeComputer.kt
@@ -2,6 +2,7 @@ package day05
 
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.math.pow
 
 class IntcodeComputer {
 
@@ -26,46 +27,75 @@ class IntcodeComputer {
         var resultAddress = 0
 
         loop@ while(opcode != OP_HALT) {
-            opcode = memory[instructionPointer]
+            opcode = memory[instructionPointer] % 100
+            val paramModes = memory[instructionPointer] / 100
 
-            val result = when(opcode) {
+            when(opcode) {
                 OP_ADD -> {
-                    val firstOperand = memory[memory[instructionPointer + 1]]
-                    val secondOperand = memory[memory[instructionPointer + 2]]
+                    val firstOperandPosition = if (paramModes / 10.0.pow(1-1).toInt() == 0) {
+                        memory[instructionPointer + 1]
+                    } else {
+                        instructionPointer + 1
+                    }
+                    val firstOperand = memory[firstOperandPosition]
+
+                    val secondOperandPosition = if (paramModes / 10.0.pow(2-1).toInt() == 0) {
+                        memory[instructionPointer + 2]
+                    } else {
+                        instructionPointer + 2
+                    }
+                    val secondOperand = memory[secondOperandPosition]
+
                     resultAddress = memory[instructionPointer + 3]
                     instructionPointer += 4
 
-                    firstOperand + secondOperand
+                    memory[resultAddress] = firstOperand + secondOperand
                 }
                 OP_MULT -> {
-                    val firstOperand = memory[memory[instructionPointer + 1]]
-                    val secondOperand = memory[memory[instructionPointer + 2]]
+                    val firstOperandPosition = if (paramModes / 10.0.pow(1-1).toInt() == 0) {
+                        memory[instructionPointer + 1]
+                    } else {
+                        instructionPointer + 1
+                    }
+                    val firstOperand = memory[firstOperandPosition]
+
+                    val secondOperandPosition = if (paramModes / 10.0.pow(2-1).toInt() == 0) {
+                        memory[instructionPointer + 2]
+                    } else {
+                        instructionPointer + 2
+                    }
+                    val secondOperand = memory[secondOperandPosition]
+
                     resultAddress = memory[instructionPointer + 3]
                     instructionPointer += 4
 
-                    firstOperand * secondOperand
+                    memory[resultAddress] = firstOperand * secondOperand
                 }
                 OP_INPUT -> {
-                    resultAddress = memory[instructionPointer + 1]
+                    resultAddress = if (paramModes / 10.0.pow(1-1).toInt() == 0) {
+                        memory[instructionPointer + 1]
+                    } else {
+                        instructionPointer + 1
+                    }
+
                     instructionPointer += 2
 
-                    inputBuffer.poll()
+                    memory[resultAddress] = inputBuffer.poll()
                 }
                 OP_OUTPUT -> {
-                    val firstOperand = memory[memory[instructionPointer + 1]]
-                    resultAddress = memory[instructionPointer + 1]
+                    val firstOperandPosition = if (paramModes / 10.0.pow(1-1).toInt() == 0) {
+                        memory[instructionPointer + 1]
+                    } else {
+                        instructionPointer + 1
+                    }
+                    val firstOperand = memory[firstOperandPosition]
                     instructionPointer += 2
 
                     outputBuffer.add(firstOperand)
-
-                    firstOperand
-
                 }
                 OP_HALT -> break@loop
                 else -> throw UnsupportedOperationException()
             }
-
-            memory[resultAddress] = result
         }
 
         return memory[0]
--- src/test/kotlin/day05/IntcodeComputerTest.kt
+++ src/test/kotlin/day05/IntcodeComputerTest.kt
@@ -25,6 +25,16 @@ class IntcodeComputerTest {
     }
 
     @Test
+    fun `ADD in immediate mode`() {
+        val opcode = ADD + 100 + 1000
+
+        assertEquals(
+            8,
+            IntcodeComputer().execute(listOf(opcode, 5, 3, 0, 99))
+        )
+    }
+
+    @Test
     fun `MULT in default positional mode`() {
         assertEquals(
             2,
@@ -33,6 +43,26 @@ class IntcodeComputerTest {
         assertEquals(
             99.0.pow(3.0).toInt(),
             IntcodeComputer().execute(listOf(MULT, 8, 8, 0, MULT, 0, 8, 0, 99))
+        )
+    }
+
+    @Test
+    fun `MULT in immediate mode`() {
+        val opcode = MULT + 100 + 1000
+
+        assertEquals(
+            20,
+            IntcodeComputer().execute(listOf(opcode, 5, 4, 0, 99))
+        )
+    }
+
+    @Test
+    fun `immediate mode should set omitted params modes to zero`() {
+        val opcode = ADD + 100 // second param in positional mode
+
+        assertEquals(
+            90,
+            IntcodeComputer().execute(listOf(opcode, -11, 0, 0, 99))
         )
     }
 
@@ -47,6 +77,17 @@ class IntcodeComputerTest {
     }
 
     @Test
+    fun `INPUT operation in immediate mode`() {
+        val computer = IntcodeComputer()
+
+        computer.registerInput(42)
+        computer.execute(listOf(INPUT + 100, 0, OUTPUT, 1, 99))
+        val got = computer.output().last()
+
+        assertEquals(    42, got)
+    }
+
+    @Test
     fun `OUTPUT operation`() {
         val computer = IntcodeComputer()
 
@@ -56,5 +97,12 @@ class IntcodeComputerTest {
         assertEquals(42, computer.output().last())
     }
 
-    // TODO: immediate mode
+    @Test
+    fun `OUTPUT operation in immediate mode`() {
+        val computer = IntcodeComputer().apply {
+            execute(listOf(OUTPUT + 100, -57, 99))
+        }
+
+        assertEquals(-57, computer.output().last())
+    }
 }
